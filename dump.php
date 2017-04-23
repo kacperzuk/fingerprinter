8c31909f006b2d8107f1b9f5038427aa4cf277e5
Kacper Żuk <kacper.b.zuk@gmail.com>
2017-04-23 17:59:56 +0200

    Debug

--- dump.php
+++ dump.php
@@ -2,8 +2,6 @@
 
 header("Access-Control-Allow-Origin: *");
 
-if ($_SERVER["REQUEST_METHOD"] != "POST") die();
-
 ini_set("display_errors", true);
 error_reporting(-1);
 
@@ -11,10 +9,9 @@ $dbopts = parse_url(getenv('DATABASE_URL'));
 $dbConn = new PDO('pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"], $dbopts["user"], $dbopts["pass"]);
 $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$q = $dbConn->query("select * from data;");
-
 echo "======\n";
 $out = fopen('php://output', 'w');
+$q = $dbConn->query("select * from data;");
 foreach($q as $row) {
     fputcsv($out, $row);
 }
