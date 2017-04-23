199496dec420fa237a0817e5c279d472af62cada
Kacper Żuk <kacper.b.zuk@gmail.com>
2017-04-23 18:00:43 +0200

    Debug

--- dump.php
+++ dump.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Content-Type: text/plain");
 
 ini_set("display_errors", true);
 error_reporting(-1);
@@ -9,11 +9,9 @@ $dbopts = parse_url(getenv('DATABASE_URL'));
 $dbConn = new PDO('pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"], $dbopts["user"], $dbopts["pass"]);
 $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo "======\n";
 $out = fopen('php://output', 'w');
 $q = $dbConn->query("select * from data;");
 foreach($q as $row) {
     fputcsv($out, $row);
 }
 fclose($out);
-echo "=======\n";
