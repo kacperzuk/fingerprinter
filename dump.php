47c38c2bb71e7ce679197a01231bddc6735d71b3
Kacper Żuk <kacper.b.zuk@gmail.com>
2017-04-23 18:07:50 +0200

    Debug

--- dump.php
+++ dump.php
@@ -1,14 +1,16 @@
 <?php
 
 header("Content-Type: text/plain");
+header("Content-Encoding: gzip");
 header('Content-Disposition: attachment; filename="fingerprinter_data.csv"');
+ob_start("ob_gzhandler");
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $dbConn = new PDO('pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"], $dbopts["user"], $dbopts["pass"]);
 $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $out = fopen('php://output', 'w');
-$rs = $dbConn->query('SELECT * FROM my_table LIMIT 0');
+$rs = $dbConn->query('select * from data limit 0');
 for ($i = 0; $i < $rs->columnCount(); $i++) {
     $col = $rs->getColumnMeta($i);
     $columns[] = $col['name'];
